650be796c2137766fc778ac55921a1b675c80f46
Ashikur Rahman Likhon <mdarlikhon317@gmail.com>
2025-02-14 15:46:21 +0600

    Creating a coupon system is created. Updated code

--- orders/views.py
+++ orders/views.py
@@ -11,14 +11,23 @@ from django.contrib.staticfiles import finders
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
+
 @staff_member_required
 def admin_order_pdf(request, order_id):
     order = get_object_or_404(Order, pk=order_id)
     html = render_to_string('orders/order/pdf.html', {'order': order})
-    response = HttpResponse(html, content_type='application/pdf')
+
+    # Create the response with content type as PDF
+    response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order_id}.pdf'
-    weasyprint.HTML(string=html).write_pdf(response),
+
+    # Generate the PDF from HTML
+    weasyprint.HTML(string=html).write_pdf(response)
+
+    # If you need to use custom CSS (optional)
     stylesheets = [weasyprint.CSS(finders.find('css/pdf.css'))]
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=stylesheets)
+
     return response
 
 def order_create(request):
